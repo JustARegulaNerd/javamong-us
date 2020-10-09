019922e863eea462cd3384f6f7b2f1b3bc564947
justaregularnerd <justaregularnerd@localhost.localdomain>
2020-10-10 08:30:45 +1000

    Initial NetBeans project creation

---
+++ JavAmongUs/src/javamongus/JavAmongUs.java
@@ -0,0 +1,15 @@
+package javamongus;
+
+/**
+ * @author justaregularnerd
+ */
+public class JavAmongUs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
